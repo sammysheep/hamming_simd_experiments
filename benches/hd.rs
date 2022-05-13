6e93a2aa012a701670c5ddde03b4cbbb6d6fab03
Samuel Shepard <vfn4@cdc.gov>
2022-05-13 10:56:29 -0400

    Clean up

--- benches/hd.rs
+++ benches/hd.rs
@@ -1,107 +1,78 @@
-
 #![feature(test)]
 use triple_accel::hamming::hamming as intrinsic_hd;
 
 extern crate test;
-use test::Bencher;
 use hamming::*;
+use test::Bencher;
 
 #[bench]
 fn scalar_hd_lg(b: &mut Bencher) {
-    b.iter(|| {
-        scalar_hamming(L1, L2)
-    });
+    b.iter(|| scalar_hamming(L1, L2));
 }
 
 #[bench]
 fn intrinsic_hd_lg(b: &mut Bencher) {
-    b.iter(|| {
-        intrinsic_hd(L1, L2)
-    });
+    b.iter(|| intrinsic_hd(L1, L2));
 }
 
 #[bench]
 fn simd_chunk_eq_hd16_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_chunk_eq_hd::<16>(L1, L2)
-    });
+    b.iter(|| simd_chunk_eq_hd::<16>(L1, L2));
 }
 
 #[bench]
 fn simd_chunk_eq_hd32_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_chunk_eq_hd::<32>(L1, L2)
-    });
+    b.iter(|| simd_chunk_eq_hd::<32>(L1, L2));
 }
 
 #[bench]
 fn simd_chunk_ne_hd16_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_chunk_ne_hd::<16>(L1, L2)
-    });
+    b.iter(|| simd_chunk_ne_hd::<16>(L1, L2));
 }
 
 #[bench]
 fn simd_chunk_ne_hd32_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_chunk_ne_hd::<32>(L1, L2)
-    });
+    b.iter(|| simd_chunk_ne_hd::<32>(L1, L2));
 }
 
 #[bench]
 fn simd_reduce_ne_hd16_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_reduce_ne_hd::<16>(L1, L2)
-    });
+    b.iter(|| simd_reduce_ne_hd::<16>(L1, L2));
 }
 
 #[bench]
 fn simd_reduce_ne_hd32_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_reduce_ne_hd::<32>(L1, L2)
-    });
+    b.iter(|| simd_reduce_ne_hd::<32>(L1, L2));
 }
 
 #[bench]
 fn simd_chunk_xor_hd16_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_chunk_xor_hd::<16>(L1, L2)
-    });
+    b.iter(|| simd_chunk_xor_hd::<16>(L1, L2));
 }
 
 #[bench]
 fn simd_chunk_xor_hd32_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_chunk_xor_hd::<32>(L1, L2)
-    });
+    b.iter(|| simd_chunk_xor_hd::<32>(L1, L2));
 }
 
 #[bench]
 fn simd_for_ne_hd16_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_for_ne_hd::<16>(L1, L2)
-    });
+    b.iter(|| simd_for_ne_hd::<16>(L1, L2));
 }
 
 #[bench]
 fn simd_for_ne_hd32_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_for_ne_hd::<32>(L1, L2)
-    });
+    b.iter(|| simd_for_ne_hd::<32>(L1, L2));
 }
 
 #[bench]
 fn simd_while_ne_hd16_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_for_ne_hd::<16>(L1, L2)
-    });
+    b.iter(|| simd_for_ne_hd::<16>(L1, L2));
 }
 
 #[bench]
 fn simd_while_ne_hd32_lg(b: &mut Bencher) {
-    b.iter(|| {
-        simd_for_ne_hd::<32>(L1, L2)
-    });
+    b.iter(|| simd_for_ne_hd::<32>(L1, L2));
 }
 
 #[bench]
@@ -111,9 +82,7 @@ fn simd_aligned_ne_hd16_lg(b: &mut Bencher) {
 
     let a2 = AlignedVec(L2.to_vec());
     let a2 = a2.get_slice();
-    b.iter(|| {
-        simd_aligned_ne_hd::<16>(a1, a2)
-    });
+    b.iter(|| simd_aligned_ne_hd::<16>(a1, a2));
 }
 
 #[bench]
@@ -124,9 +93,7 @@ fn simd_aligned_ne_hd32_lg(b: &mut Bencher) {
     let a2 = AlignedVec(L2.to_vec());
     let a2 = a2.get_slice();
 
-    b.iter(|| {
-        simd_aligned_ne_hd::<32>(a1, a2)
-    });
+    b.iter(|| simd_aligned_ne_hd::<32>(a1, a2));
 }
 
 #[bench]
@@ -136,9 +103,7 @@ fn simd_aligned_eq_hd16_lg(b: &mut Bencher) {
 
     let a2 = AlignedVec(L2.to_vec());
     let a2 = a2.get_slice();
-    b.iter(|| {
-        simd_aligned_eq_hd::<16>(a1, a2)
-    });
+    b.iter(|| simd_aligned_eq_hd::<16>(a1, a2));
 }
 
 #[bench]
@@ -149,7 +114,5 @@ fn simd_aligned_eq_hd32_lg(b: &mut Bencher) {
     let a2 = AlignedVec(L2.to_vec());
     let a2 = a2.get_slice();
 
-    b.iter(|| {
-        simd_aligned_eq_hd::<32>(a1, a2)
-    });
+    b.iter(|| simd_aligned_eq_hd::<32>(a1, a2));
 }
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,25 @@
 //use std::cmp::min;
 use hamming::*;
 
-
 fn main() {
     let n = S1.len();
     let d = scalar_hamming(S1, S2);
     println!("Scalar: {d} of {n}");
 
     let d = simd_chunk_ne_hd::<32>(S1, S2);
-    println!("simd_chunk_ne_hd::<32>: {d} of {n}");  
+    println!("simd_chunk_ne_hd::<32>: {d} of {n}");
 
     let d = simd_chunk_ne_hd::<16>(S1, S2);
-    println!("simd_chunk_ne_hd::<16>: {d} of {n}");  
+    println!("simd_chunk_ne_hd::<16>: {d} of {n}");
 
     let d = simd_reduce_ne_hd::<16>(S1, S2);
     println!("simd_reduce_ne_hd::<16>: {d} of {n}");
 
     let d = simd_chunk_eq_hd::<32>(S1, S2);
-    println!("simd_chunk_eq_hd::<32>: {d} of {n}");  
+    println!("simd_chunk_eq_hd::<32>: {d} of {n}");
 
     let d = simd_chunk_eq_hd::<16>(S1, S2);
-    println!("simd_chunk_eq_hd::<16>: {d} of {n}"); 
+    println!("simd_chunk_eq_hd::<16>: {d} of {n}");
 
     let a1 = AlignedVec(S1.to_vec());
     let a1 = a1.get_slice();
@@ -31,41 +30,41 @@ fn main() {
     let a2 = a2.get_slice();
 
     let d = simd_aligned_ne_hd::<16>(a1, a2);
-    println!("simd_aligned_ne_hd::<16> {d} of {n}"); 
-   
+    println!("simd_aligned_ne_hd::<16> {d} of {n}");
+
     let d = simd_aligned_eq_hd::<16>(a1, a2);
-    println!("simd_aligned_eq_hd::<16> {d} of {n}"); 
+    println!("simd_aligned_eq_hd::<16> {d} of {n}");
 
     let n = L1.len();
     let d = scalar_hamming(L1, L2);
     println!("\nScalar: {d} of {n}");
 
     let d = simd_chunk_ne_hd::<32>(L1, L2);
-    println!("simd_chunk_ne_hd::<32>: {d} of {n}");  
+    println!("simd_chunk_ne_hd::<32>: {d} of {n}");
 
     let d = simd_chunk_ne_hd::<16>(L1, L2);
-    println!("simd_chunk_ne_hd::<16>: {d} of {n}");  
+    println!("simd_chunk_ne_hd::<16>: {d} of {n}");
 
     let d = simd_reduce_ne_hd::<32>(L1, L2);
     println!("simd_reduce_ne_hd::<32>: {d} of {n}");
 
     let d = simd_chunk_eq_hd::<32>(L1, L2);
-    println!("simd_chunk_eq_hd::<32>: {d} of {n}");  
+    println!("simd_chunk_eq_hd::<32>: {d} of {n}");
 
     let d = simd_chunk_eq_hd::<16>(L1, L2);
-    println!("simd_chunk_eq_hd::<16>: {d} of {n}"); 
+    println!("simd_chunk_eq_hd::<16>: {d} of {n}");
 
     let d = simd_chunk_xor_hd::<16>(L1, L2);
-    println!("simd_chunk_xor_hd::<16>: {d} of {n}"); 
+    println!("simd_chunk_xor_hd::<16>: {d} of {n}");
 
     let d = simd_chunk_xor_hd::<32>(L1, L2);
-    println!("simd_chunk_xor_hd::<32>: {d} of {n}"); 
+    println!("simd_chunk_xor_hd::<32>: {d} of {n}");
 
     let d = simd_for_ne_hd::<32>(L1, L2);
-    println!("simd_for_ne_hd::<32>: {d} of {n}"); 
+    println!("simd_for_ne_hd::<32>: {d} of {n}");
 
     let d = simd_while_ne_hd::<32>(L1, L2);
-    println!("simd_while_ne_hd::<32>: {d} of {n}"); 
+    println!("simd_while_ne_hd::<32>: {d} of {n}");
 
     let a1 = AlignedVec(L1.to_vec());
     let a1 = a1.get_slice();
@@ -74,9 +73,8 @@ fn main() {
     let a2 = a2.get_slice();
 
     let d = simd_aligned_ne_hd::<32>(a1, a2);
-    println!("simd_aligned_ne_hd::<32> {d} of {n}"); 
+    println!("simd_aligned_ne_hd::<32> {d} of {n}");
 
     let d = simd_aligned_eq_hd::<32>(a1, a2);
-    println!("simd_aligned_eq_hd::<32> {d} of {n}"); 
-
+    println!("simd_aligned_eq_hd::<32> {d} of {n}");
 }
